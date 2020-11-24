905325a6c349df625f836d7503ca317e48660700
Julio Carrion <julio@itic.com.pe>
2020-11-24 00:44:42 -0500

    Avance parcial

---
+++ app/src/main/java/pe/pucp/dduu/tel306/Entity/Respuestas.java
@@ -0,0 +1,22 @@
+package pe.pucp.dduu.tel306.Entity;
+
+public class Respuestas {
+    private int id;
+    private String answerText;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+}
---
+++ app/src/main/java/pe/pucp/dduu/tel306/QuestionsActivity.java
@@ -0,0 +1,14 @@
+package pe.pucp.dduu.tel306;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+    }
+}
