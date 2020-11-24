d9eea7c9f0ae511f3428196bf2fdcbccd90916f0
Royer <r.yangali@pucp.pe>
2020-11-24 11:39:34 -0500

    royer - agregando logica inicial en main activity

--- app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
+++ app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //VALIDANDO SI HAY O NO UN ARCHIVO .JSON con datos de usuario en memoria local
+        try (FileInputStream fileInputStream = openFileInput("archivoAleerConObjetos.json");
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            if (fileInputStream.available() > 0) {  //es decir, si es que hay bytes para leer...
+                ///mandar a question activity
+                Intent intent = new Intent(MainActivity.this,QuestionsActivity.class);
+                startActivity(intent);
+            }else{ ///cuando no existe el archivo JSON
+                ///abrir login
+                agregarLoginFragment();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void agregarLoginFragment(View view){
+    public void agregarLoginFragment(){
         LoginFragment loginFragment = LoginFragment.newInstance();
         FragmentManager supportFragmentManager = getSupportFragmentManager();
 
@@ -25,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    public void agregarRegisterFragment(View view){
+    public void agregarRegisterFragment(){
         RegisterFragment registerFragment = RegisterFragment.newInstance();
         FragmentManager supportFragmentManager = getSupportFragmentManager();
 
@@ -33,5 +55,10 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.add(R.id.principalFragmentContainer,registerFragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+        ///////////////////////////////
+
+
+
+
     }
 }
