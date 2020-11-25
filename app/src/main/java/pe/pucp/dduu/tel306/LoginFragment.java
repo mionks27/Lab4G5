001c9351cef120d8dab5c6b2d819d7d0893567c3
Fernando <a20162001@pucp.edu.pe>
2020-11-24 23:17:07 -0500

    CASI TODO

--- app/src/main/java/pe/pucp/dduu/tel306/LoginFragment.java
+++ app/src/main/java/pe/pucp/dduu/tel306/LoginFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -36,6 +37,8 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 }
--- app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
+++ app/src/main/java/pe/pucp/dduu/tel306/MainActivity.java
@@ -1,15 +1,18 @@
 package pe.pucp.dduu.tel306;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -23,6 +26,11 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
 
 import java.io.FileInputStream;
@@ -35,27 +43,96 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View loginFragment = findViewById(R.id.principalFragmentContainer);
+        try(FileInputStream fileInputStream = openFileInput("archivo.json");
+        ) {
+            //JULIO SI INICIA Y EL ARCHIVO YA ESTA
+            //GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            //ESTO TE PUEDE SERVIR
+            //String id = obtenerID();
 
-        /*
-        Button buttonLogin = loginFragment.findViewById(R.id.button2);
-        if(buttonLogin != null){
-            buttonLogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    EditText editText = loginFragment.findViewById(R.id.editTextTextPersonName);
-                    String email = editText.getText().toString();
-                    EditText editText1 = loginFragment.findViewById(R.id.editTextTextPersonName2);
-                    String password = editText1.getText().toString();
-
-                    String data = "{\"email\":" + email + ",\"password\":" + password + "\"}";
-                    Log.d("infoApp",data);
-                    Submit(data);
-                }
-            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            agregarLoginFragment();
         }
-        */
     }
+    public void volvemosAlLogin(View view){
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        //AHORA BUSCAMOS EL FRAGMENTO SI REALMENTE EXISTE
+        RegisterFragment registerFragment = (RegisterFragment) supportFragmentManager.findFragmentById(R.id.principalFragmentContainer);
+        //SI LO ENCUENTRA EL blankFragment ES DIFERENTE DE NULO, ES DECIR QUE EL FRAGMENTO YA ESTA AHI, POR LO TANTO HAY QUE BORRARLO
+        if (registerFragment != null){
+            //QUE BORRE EL FRAGMENTO
+            FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+            fragmentTransaction.remove(registerFragment);
+            fragmentTransaction.commit();
+        }
+        agregarLoginFragment();
+    }
+
+    public void vamosAlRegister(View view){
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        //AHORA BUSCAMOS EL FRAGMENTO SI REALMENTE EXISTE
+        LoginFragment loginFragment = (LoginFragment) supportFragmentManager.findFragmentById(R.id.principalFragmentContainer);
+        //SI LO ENCUENTRA EL blankFragment ES DIFERENTE DE NULO, ES DECIR QUE EL FRAGMENTO YA ESTA AHI, POR LO TANTO HAY QUE BORRARLO
+        if (loginFragment != null){
+            //QUE BORRE EL FRAGMENTO
+            FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+            fragmentTransaction.remove(loginFragment);
+            fragmentTransaction.commit();
+        }
+        agregarRegisterFragment();
+    }
+    public void consultaLogin(View view){
+        EditText editText1 = findViewById(R.id.editTextEmailLogin);
+        String email = editText1.getText().toString();
+        EditText editText2 = findViewById(R.id.editTextPasswordLogin);
+        String password = editText2.getText().toString();
+        //String email = "todoMeLlegaAlPNCHO@pucp.pe";
+        //String password = "321";
+        Log.d("infoApp","infoApp : " + email);
+        Log.d("infoApp","infoApp : " + password);
+        String data = "{\"email\": \""+ email +"\",\"password\": \"" + password + "\"}";
+        Log.d("infoApp"," JSON LOGIN : " + data);
+        SubmitLogin(data);
+
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+
+        //AHORA BUSCAMOS EL FRAGMENTO SI REALMENTE EXISTE
+        LoginFragment loginFragment = (LoginFragment) supportFragmentManager.findFragmentById(R.id.principalFragmentContainer);
+
+        //SI LO ENCUENTRA EL blankFragment ES DIFERENTE DE NULO, ES DECIR QUE EL FRAGMENTO YA ESTA AHI, POR LO TANTO HAY QUE BORRARLO
+
+        if (loginFragment != null){
+            //QUE BORRE EL FRAGMENTO
+            FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+            fragmentTransaction.remove(loginFragment);
+            fragmentTransaction.commit();
+        }
+        Log.d("infoApp"," YA HAY ARCHIVO ");
+        //JULIO SI SE LOGEA BIEN:
+        //GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        //ESTO TE PUEDE SERVIR
+        //String id = obtenerID();
+
+
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void registrarUsuario(View view){
+        EditText editText1 = findViewById(R.id.editTextNombreUsuario);
+        String name = editText1.getText().toString();
+        EditText editText2 = findViewById(R.id.editTextEmail);
+        String email = editText2.getText().toString();
+        EditText editText3 = findViewById(R.id.editTextContrasena);
+        String password = editText3.getText().toString();
+        //String email = "todoMeLlegaAlPNCHO@pucp.pe";
+        //String password = "321";
+        //Log.d("infoApp","infoApp : " + email);
+        //Log.d("infoApp","infoApp : " + password);
+        String data = "{\"name\":\"" + name + "\",\"email\":\"" + email + "\",\"password\":\"" + password + "\"}";
+        //Log.d("infoApp"," JSON LOGIN : " + data);
+        SubmitRegister(data);
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //CON ESTO SE MUESTRA EL FRAGMENT DE LOGIN
     public void agregarLoginFragment(){
         LoginFragment loginFragment = LoginFragment.newInstance();
@@ -63,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.principalFragmentContainer,loginFragment);
-        //fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -76,32 +153,31 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.add(R.id.principalFragmentContainer,registerFragment);
         //fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-        ///////////////////////////////
-
-
-
-
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //JSON
-    private RequestQueue requestQueue;
-    private void Submit(String data)
+    private RequestQueue requestQueueLogin;
+    private void SubmitLogin(String data)
     {
         final String savedata= data;
         String URL="http://34.236.191.118:3000/api/v1/users/login";
 
-        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueueLogin = Volley.newRequestQueue(getApplicationContext());
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
                     JSONObject objres = new JSONObject(response);
-                    Toast.makeText(getApplicationContext(),objres.toString(),Toast.LENGTH_LONG).show();
-                    Log.d("infoApp","consulta CORRECTA");
-                    Log.d("infoApp","ID : " + objres.get("id").toString());
-                    Log.d("infoApp","NAME : " + objres.get("name").toString());
-                    Log.d("infoApp","EMAIL : " + objres.get("email").toString());
-                    Log.d("infoApp","PASSWORD : " + objres.get("password").toString());
+                    String mensaje = "Usuario Logeado Correctamente";
+                    Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_LONG).show();
+
+                    try(FileOutputStream fileOutputStream = openFileOutput("archivo.json", Context.MODE_PRIVATE);
+                        FileWriter fileWriter = new FileWriter(fileOutputStream.getFD());
+                    ){
+                        fileWriter.write(objres.toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
                 } catch (JSONException e) {
                     Toast.makeText(getApplicationContext(),"Server Error",Toast.LENGTH_LONG).show();
@@ -130,7 +206,81 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-        requestQueue.add(stringRequest);
+        requestQueueLogin.add(stringRequest);
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private RequestQueue requestQueueRegister;
+    private void SubmitRegister(String data)
+    {
+        final String savedata= data;
+        String URL="http://34.236.191.118:3000/api/v1/users/new";
+
+        requestQueueRegister = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.equalsIgnoreCase("true")){
+                    Toast.makeText(getApplicationContext(),"Usuario Registrado Correctamente ",Toast.LENGTH_LONG).show();
+                }else if(response.equalsIgnoreCase("false")){
+                    Toast.makeText(getApplicationContext(),"Este Usuario Ya Se Registró Anteriormente,PASAR AL LOGIN POR FAVOR",Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d("infoApp","LA KGASTE");
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return savedata == null ? null : savedata.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    //Log.d("Unsupported Encoding while trying to get the bytes", data);
+                    Log.d("infoApp","ABC");
+                    return null;
+                }
+            }
+        };
+        requestQueueRegister.add(stringRequest);
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public String obtenerID(){
+        String id = null;
+        try(FileInputStream fileInputStream = openFileInput("archivo.json");
+            FileReader fileReader = new FileReader(fileInputStream.getFD());// FileReader lee caracter, no es muy practico asi q lo complmentamos con Buffer
+            BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line = null;
+            while ((line = bufferedReader.readLine())!=null){
+                try {
+                    JSONObject objres = new JSONObject(line);
+                    Log.d("infoLine","ID : " + objres.get("id").toString());
+                    Log.d("infoLine","NAME : " + objres.get("name").toString());
+                    Log.d("infoLine","EMAIL : " + objres.get("email").toString());
+                    Log.d("infoLine","PASSWORD : " + objres.get("password").toString());
+                    Log.d("infoLine","TOKEN : " + objres.get("token").toString());
+                    Log.d("infoLine","CREATEDAT : " + objres.get("createdAt").toString());
+                    Log.d("infoLine","UPDATEDAT : " + objres.get("updatedAt").toString());
+                    Log.d("infoLine","QUESTIONS : " + objres.get("questions").toString());
+
+                    id = objres.get("id").toString();
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(),"Server Error",Toast.LENGTH_LONG).show();
+                    Log.d("infoLine","F");
+                    Log.d("infoApp",line);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d("infoLine","NO EXISTE EL ARCHIVO");
+        }
+        return id;
+    }
 }
